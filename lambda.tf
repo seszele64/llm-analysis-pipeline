aff862ff60032a05a46b99f53321fc5d063e0bee
seszele64 <sebastian@szelewi.cz>
2025-01-30 00:23:44 +0100

    feat: add DynamoDB stream trigger for sentiment analysis
    - Configure DynamoDB stream on olist_order_reviews table
    - Add Lambda event source mapping to trigger sentiment analyzer
    - Add IAM policy for Lambda to access DynamoDB streams
    - Set up AWS Secrets Manager for Groq API key storage

--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,12 @@
+resource "aws_secretsmanager_secret" "groq_api_key" {
+  name = "groq-api-key"
+}
+
+resource "aws_secretsmanager_secret_version" "groq_api_key_version" {
+  secret_id     = aws_secretsmanager_secret.groq_api_key.id
+  secret_string = "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"
+}
+
 resource "aws_lambda_function" "sentiment_analyzer" {
   function_name = "sentiment_analyzer"
   handler       = "handler.lambda_handler"
@@ -11,7 +20,7 @@ resource "aws_lambda_function" "sentiment_analyzer" {
 
   environment {
     variables = {
-      SECRET_NAME = "groq_api_key"
+      SECRET_NAME = "groq-api-key"
     }
   }
 }
@@ -29,6 +38,48 @@ resource "aws_iam_role_policy" "lambda_secrets_manager" {
           "secretsmanager:GetSecretValue"
         ]
         Resource = "*"
+      }
+    ]
+  })
+}
+
+# DynamoDB Stream Trigger Configuration
+resource "aws_dynamodb_table" "olist_order_reviews" {
+  name           = "olist_order_reviews"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "review_id"
+
+  attribute {
+    name = "review_id"
+    type = "S"
+  }
+
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+}
+
+resource "aws_lambda_event_source_mapping" "dynamodb_trigger" {
+  event_source_arn  = aws_dynamodb_table.olist_order_reviews.stream_arn
+  function_name     = aws_lambda_function.sentiment_analyzer.arn
+  starting_position = "LATEST"
+}
+
+resource "aws_iam_role_policy" "lambda_dynamodb_stream" {
+  name = "lambda_dynamodb_stream_policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetRecords",
+          "dynamodb:GetShardIterator",
+          "dynamodb:DescribeStream",
+          "dynamodb:ListStreams"
+        ]
+        Resource = aws_dynamodb_table.olist_order_reviews.stream_arn
       }
     ]
   })
