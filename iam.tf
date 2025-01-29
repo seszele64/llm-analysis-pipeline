c6f9b56a949f72dc031133175b382c83447abc40
seszele64 <sebastian@szelewi.cz>
2025-01-29 16:21:32 +0100

    feat: Initialize AWS infrastructure for LLM analysis pipeline
    - Create input and output S3 buckets for data storage
    - Set up DynamoDB table for classification results
    - Configure IAM role for Lambda function execution

---
+++ iam.tf
@@ -0,0 +1,52 @@
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "classification_lambda_policy"
+  description = "Policy for Lambda function executing classification"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject"
+        ]
+        Resource = [
+          aws_s3_bucket.input_bucket.arn,
+          "${aws_s3_bucket.input_bucket.arn}/*",
+          aws_s3_bucket.output_bucket.arn,
+          "${aws_s3_bucket.output_bucket.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:Query",
+          "dynamodb:DescribeContinuousBackups",
+          "dynamodb:DescribeTable",
+          "dynamodb:DescribeTimeToLive",
+          "dynamodb:ListTagsOfResource"
+        ]
+        Resource = aws_dynamodb_table.classification_results.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
